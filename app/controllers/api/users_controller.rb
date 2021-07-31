735e3df70788764cb6564dfacf1a8e722529aa40
lbrilli <lbrilli11@gmail.com>
2021-07-31 13:27:58 -0600

    complete application controller, users_controller, and user model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,18 @@
 class Api::UsersController < ApplicationController
+    
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            # login user
+            render "api/calendars" #up for debate... should I add a default calendar to each user? could a user show page really be a page of calendars?
+        else
+            render :json @user.errors.full_messages, status: 422
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def login(user)
+        user.reset_session_token!
+        session[:session_token] = user.session_token
+        @current_user = user
+
+    def log_out
+        current_user.reset_session_token! if logged_in?
+        session[:session_token] = nil
+        @current_user = nil
+
+    def require_logged_in
+        unless logged_in?
+            render json: { base: ['inavlid credentials'] }, status: 401
+
 end
