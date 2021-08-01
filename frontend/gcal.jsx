068dc1152f56672e3aededf0fc43ecbcba34d3ec
lbrilli <lbrilli11@gmail.com>
2021-08-01 16:42:26 -0600

    wrote session_actions, session_reducer, users_reducer and made file for session_errors_reducer

--- frontend/gcal.jsx
+++ frontend/gcal.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import log_in from './util/session_api_util'
+import log_out from './util/session_api_util'
+import signup from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root')
+    
+    window.signup = signup;
+    window.log_in = log_in;
+    window.log_out = log_out;
 
     ReactDOM.render(<h1>React is working!</h1>, root);
 })
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,17 @@
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from "../actions/session_actions";
+
+const SessionReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({id: null});
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {id: action.currentUser.id};
+        case LOGOUT_CURRENT_USER:
+            return nextState
+        default:
+            return state;
+    }
+}
+
+export default SessionReducer;
